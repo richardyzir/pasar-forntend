fe6f3feff774d3c3742e3cd820db610e8332c2bf
richardyzir <richard.job307@gmail.com>
2026-05-29 00:54:53 +0800

    fix header apk

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,15 @@
-self.addEventListener("install", () => {});
-self.addEventListener("fetch", () => {});
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("fofimart").then((cache) => {
+      return cache.addAll(["/", "/manifest.json"]);
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
